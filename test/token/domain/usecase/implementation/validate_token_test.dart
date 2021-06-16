da3541389184ce2e5bf30d9482e109952dcc4d04
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-15 21:54:15 -0300

    Add test for a validate_token implement

---
+++ test/token/domain/usecase/implementation/validate_token_test.dart
@@ -0,0 +1,32 @@
+import 'package:dart_jwt/core/error/failure.dart';
+import 'package:dart_jwt/token/domain/errors/failures.dart';
+import 'package:dart_jwt/token/domain/repository/validate_token_repository.dart';
+import 'package:dart_jwt/token/domain/usecase/implementation/validate_token.dart';
+import 'package:dartz/dartz.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class ValidateTokenRepositgoryMock extends Mock implements IValidateTokenRepository {}
+
+main() {
+  final repository = ValidateTokenRepositgoryMock();
+  final usecase = ValidateToken(repository: repository);
+
+  test('Must be return a json response', () async {
+    when(() => usecase()).thenAnswer((_) async => Right(<String, dynamic>{}));
+
+    var result = await usecase();
+    expect(result.isRight(), true);
+    expect(result, isA<Right<Failure, Map<String, dynamic>>>());
+    verify(() => repository.validateToken());
+  });
+
+  test('Must be return a failure', () async {
+    when(() => usecase()).thenAnswer((_) async => Left(JWTExpiredFailure()));
+
+    var result = await usecase();
+    expect(result.isLeft(), true);
+    expect(result, isA<Left<Failure, Map<String, dynamic>>>());
+    verify(() => repository.validateToken());
+  });
+}
