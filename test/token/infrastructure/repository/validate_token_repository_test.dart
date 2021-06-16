48247799411b85f81ed6933126f36316d9212eb3
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2021-06-16 15:01:36 -0300

    Add tests of ValidateTokenRepository implementation

---
+++ test/token/infrastructure/repository/validate_token_repository_test.dart
@@ -0,0 +1,21 @@
+import 'package:dart_jwt/core/error/failure.dart';
+import 'package:dart_jwt/core/text/token.dart';
+import 'package:dart_jwt/token/infrastructure/repository/validate_token_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final repository = ValidateTokenRepository();
+
+  test('Must be return a json Map<String, dynamic>', () async {
+    var result = await repository.validateToken(validToken);
+    expect(result.isRight(), true);
+    expect(result, isA<Right<Failure, Map<String, dynamic>>>());
+  });
+
+  test('Must be return a JWTExpiredFailure', () async {
+    var result = await repository.validateToken(invalidToken);
+    expect(result.isLeft(), true);
+    expect(result, isA<Left<Failure, Map<String, dynamic>>>());
+  });
+}
